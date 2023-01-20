100afdfa4e5e2d9be53b78de45937f8032d76153
Ana Sanchez <ana.sanchezcvr@gmail.com>
2023-01-20 08:52:39 -0600

    server clases

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,25 +1,28 @@
-import { Router } from "express"
+import { Router } from "express";
+import { isUser } from "../utils/middleware.js";
 
 const categoryRoutes = Router()
 
-categoryRoutes.get("/", (req,res) => {
-    res.send("GetAllCategories")
+categoryRoutes.get("/", (req, res) => {
+    res.send("getAllCategories")
+})
+categoryRoutes.get("/:id", (req, res) => {
+    res.send({ getlCategoryById: req.params.id })
 })
 
-categoryRoutes.get("/:id", (req,res) => {
-    res.send("GetCategoriesById")
+categoryRoutes.use(isUser)
+
+categoryRoutes.post("/", (req, res) => {
+    res.send("createCategory")
 })
 
-categoryRoutes.post("/", (req,res) => {
-    res.send("CreateCategory")
+categoryRoutes.put("/", (req, res) => {
+    res.send("updateCategory")
 })
 
-categoryRoutes.put("/", (req,res) => {
-    res.send("UpdateCategories")
-})
-
-categoryRoutes.delete("/", (req,res) => {
-    res.send("DeleteCategory")
+categoryRoutes.delete("/", (req, res) => {
+    res.send("deleteCategory")
 })
 
 export default categoryRoutes
+
